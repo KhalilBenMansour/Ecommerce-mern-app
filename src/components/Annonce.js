ba79336321b38ab2950125d59b4c7f2a12b5c15c
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-01-17 14:23:15 +0100

    home->navbar-slider

---
+++ src/components/Annonce.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+`;
+
+const Annonce = () => {
+  return <Container>Super Deal,Free Shipping on Orders over $50</Container>;
+};
+
+export default Annonce;
